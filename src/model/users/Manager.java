66178efb187013049be4f34450fb33d9864baddc
AmiraHaiba <maro.haiba@gmail.com>
2019-05-10 01:24:06 +0200

    Added manager class.

--- src/model/users/Manager.java
+++ src/model/users/Manager.java
@@ -1,5 +1,18 @@
 package model.users;
 
-public class Manager implements User {
+import com.mysql.jdbc.ResultSet;
 
+import model.functionalities.GetReports;
+
+public class Manager implements User {
+	private GetReports gr = new GetReports();
+	public ResultSet getNumberOfNewPatients () {
+		return gr.getNumberOfNewPatients();
+	}
+	public ResultSet getNumberOfNewAppointments () {
+		return gr.getNumberOfNewAppointments();
+	}
+	public ResultSet getTotalPricesOfEachMedicine () {
+		return gr.getTotalPricesOfEachMedicine();
+	}
 }
